b2d19a022085d95b4b408cfb5a72699e191588e4
Syed shazama <syedshazama123@gmail.com>
2025-01-28 14:14:13 +0530

    cleared

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import {
-    clearOrderHistory,
     createOrder,
     getUserOrders
 } from '../controllers/orderController.js';
@@ -11,7 +10,6 @@ const router = express.Router();
 
 router.post('/createorder', authMiddleware, createOrder);
 router.get('/getorder', authMiddleware, getUserOrders);
-router.delete('/delete/:id', authMiddleware, clearOrderHistory);
 
 export default router;
 
